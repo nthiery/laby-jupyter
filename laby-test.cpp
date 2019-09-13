eca4744cb4a6cb8763f1ed1b2532d96e02287022
Nicolas M. Thiéry <nthiery@users.sf.net>
2019-09-13 10:32:59 +0200

    ASSERTEQ: format standard pour les erreurs

--- laby-test.cpp
+++ laby-test.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 #define ASSERT(test) if (!(test)) cout << "Test failed in file " << __FILE__ << " line " << __LINE__ << ": " #test << endl
-#define ASSERTEQ(obtained, expected) if (obtained != expected) cout << "Test failed in file " << __FILE__ << " line " << __LINE__ << ":\nExpected:\n" << expected << "\nGot:\n" << obtained << endl
+#define ASSERTEQ(obtained, expected) if (obtained != expected) cout << __FILE__ << ":" << __LINE__ << ": Test failed:\nExpected:\n" << expected << "\nGot:\n" << obtained << endl
 
 const string s =
         u8"o o o o o x o\n"
