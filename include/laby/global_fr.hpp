b43a72462ff915e71c4403ef6b109891e80ce4af
Disatiler <36360389+Disatiler@users.noreply.github.com>
2020-01-27 21:07:07 +0100

    Update global_fr.hpp

--- include/laby/global_fr.hpp
+++ include/laby/global_fr.hpp
@@ -22,6 +22,7 @@ auto gauche()  { return app -> gauche();  }
 auto pose()    { return app -> pose();    }
 auto prend()   { return app -> prend();   }
 auto seme()    { return app -> sow(); }
+auto trace()   { return app -> sow_steps();}
 auto ouvre()   { return app -> ouvre();   }
 auto regarde() { return app -> regarde(); }
 bool a_gagne() { return app -> won();     }
