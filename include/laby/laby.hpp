13e668793ad5e6c43685d15b112d7a2455d3197a
Disatiler <36360389+Disatiler@users.noreply.github.com>
2020-07-22 17:11:39 +0200

    Update laby.hpp
    Passage de 1000 à 10 000 étapes.

--- include/laby/laby.hpp
+++ include/laby/laby.hpp
@@ -491,7 +491,7 @@ class Player {
 
     void set_value(Labyrinth value) {
         if ( history.size() > 10000 )
-            throw std::runtime_error("Votre programme a pris plus de 1000 étapes");
+            throw std::runtime_error("Votre programme a pris plus de 10000 étapes");
         history.push_back(value);
         if (not timer.running() and time == history.size() - 2 ) {
             time++;
